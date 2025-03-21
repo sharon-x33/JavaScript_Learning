c6e3b180099a149bffd91e6f2a206a37bebe5545
sharon-x33 <sharonx330@gmail.com>
2025-03-21 14:01:48 +0530

    object

---
+++ Objects: the basics/4.1 Objects/1.js
@@ -0,0 +1,7 @@
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
+
+alert(user.surname);
---
+++ Objects: the basics/4.1 Objects/emptiness.js
@@ -0,0 +1,12 @@
+
+let schedule = {};
+alert(isEmpty(schedule)); 
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); 
+
+function isEmpty(obj){
+    for (let key in obj){
+        return false;
+    }
+    return true;
+}
