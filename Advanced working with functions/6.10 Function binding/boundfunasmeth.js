35672f9c918658fc9396c8d886962d3b8cff7ca5
sharon-x33 <sharonx330@gmail.com>
2025-03-28 17:48:12 +0530

    completed class inheritance

---
+++ Advanced working with functions/6.10 Function binding/boundfunasmeth.js
@@ -0,0 +1,9 @@
+function f() {
+    alert( this ); // ?
+  }
+  
+  let user = {
+    g: f.bind(null)
+  };
+  
+  user.g();
---
+++ Advanced working with functions/6.10 Function binding/funpropafterbind.js
@@ -0,0 +1,10 @@
+function sayHi() {
+    alert( this.name );
+  }
+  sayHi.test = 5;
+  
+  let bound = sayHi.bind({
+    name: "John"
+  });
+  
+  alert( bound.test ); // what will be the output? why?
