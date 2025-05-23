708a8464e7fd25f963f2499b6e18621dc2d4924c
sharon-x33 <sharonx330@gmail.com>
2025-05-23 12:45:39 +0530

    .

--- 5_JavaScript_30/19 - Webcam Fun/scripts.js
+++ 5_JavaScript_30/19 - Webcam Fun/scripts.js
@@ -48,18 +48,18 @@ function takePhoto() {
 
 function redEffect(pixels) {
   for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; 
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; 
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; 
   }
   return pixels;
 }
 
 function rgbSplit(pixels) {
   for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+    pixels.data[i - 150] = pixels.data[i + 0]; 
+    pixels.data[i + 500] = pixels.data[i + 1]; 
+    pixels.data[i - 550] = pixels.data[i + 2]; 
   }
   return pixels;
 }
